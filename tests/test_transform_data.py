eb2051fab2b1ec2aa5ebf49a9161222b1c48ab34
Vinícius Costa <38189783+viniciusgcto@users.noreply.github.com>
2025-03-30 01:03:29 -0300

    Update test_transform_data.py

--- tests/test_transform_data.py
+++ tests/test_transform_data.py
@@ -1,38 +1,35 @@
-import unittest
 from pyspark.sql import SparkSession
 from scripts.transform_data import fetch_and_create_dataframe
-from pyspark.sql.functions import col
 
-class TestTransformData(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls):
-        cls.spark = SparkSession.builder.appName("TestApp").getOrCreate()
+def test_fetch_and_create_dataframe():
+    spark = SparkSession.builder.appName("TestApp").getOrCreate()
 
-    @classmethod
-    def tearDownClass(cls):
-        cls.spark.stop()
+    # Executa a função para criar os DataFrames
+    fetch_and_create_dataframe()
 
-    def test_fetch_and_create_dataframe(self):
-        # Executa a função para criar os DataFrames
-        fetch_and_create_dataframe()
+    # Verificar se os DataFrames foram criados corretamente
+    bronze_df = spark.read.json("gs://bees_case/bronze_breweries")
+    assert bronze_df is not None
+    assert bronze_df.count() > 0  # Verificar se há dados
 
-        # Verificar se os DataFrames foram criados corretamente
-        bronze_df = self.spark.read.json("gs://seu-bucket/bronze_breweries")
-        self.assertIsNotNone(bronze_df)
-        self.assertGreater(bronze_df.count(), 0) # Verificar se há dados
+    silver_df = spark.read.parquet("gs://bees_case/silver_breweries")
+    assert silver_df is not None
+    assert silver_df.count() > 0
 
-        silver_df = self.spark.read.parquet("gs://seu-bucket/silver_breweries")
-        self.assertIsNotNone(silver_df)
-        self.assertGreater(silver_df.count(), 0)
+    gold_df = spark.read.parquet("gs://bees_case/gold_breweries")
+    assert gold_df is not None
+    assert gold_df.count() > 0
 
-        gold_df = self.spark.read.parquet("gs://seu-bucket/gold_breweries")
-        self.assertIsNotNone(gold_df)
-        self.assertGreater(gold_df.count(), 0)
+    # Verificar se as colunas esperadas estão presentes
+    assert "id" in bronze_df.columns
+    assert "name" in silver_df.columns
+    assert "count" in gold_df.columns
 
-        # Verificar se as colunas esperadas estão presentes
-        self.assertIn("id", bronze_df.columns)
-        self.assertIn("name", silver_df.columns)
-        self.assertIn("count", gold_df.columns)
+    # verificação de partição
+    assert "state=" in silver_df.inputFiles()[0]
+    assert "state=" in gold_df.inputFiles()[0]
 
-        # Verificar se a partição por estado foi feita corretamente
-        self.assertIn("state", silver_df.rdd.getNumPartitions())
+    spark.stop()
+
+# Executa o teste
+test_fetch_and_create_dataframe()
